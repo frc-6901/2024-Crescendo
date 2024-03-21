85f8f97f80ca1fe525de8a9d1e3eb7f5f0c40275
frc6901 <frc6901@gmail.com>
2024-03-21 15:42:20 -0500

    pathplanner auton

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -161,10 +161,10 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  // public Command getAutonomousCommand() {
-  //   // return new PathPlannerAuto("Example Auto");
-  //   return autoChooser.getSelected();
-  // }
+  public Command getAutonomousCommand() {
+    // return new PathPlannerAuto("Example Auto");
+    return autoChooser.getSelected();
+  }
 
   // public Command getAutonomousCommand() {
   //   // SequentialCommandGroup MiddleShoot = new SequentialCommandGroup(
@@ -231,44 +231,45 @@ public class RobotContainer {
   //   // return driveForward;
   //   return null;
   //   }
-  public Command getAutonomousCommand() {
-        // 1. Create trajectory settings
-        TrajectoryConfig trajectoryConfig = new TrajectoryConfig(
-                AutoConstants.kMaxSpeedMetersPerSecond,
-                AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-                        .setKinematics(DriveConstants.kDriveKinematics);
+  
+  // public Command getAutonomousCommand() {
+  //       // 1. Create trajectory settings
+  //       TrajectoryConfig trajectoryConfig = new TrajectoryConfig(
+  //               AutoConstants.kMaxSpeedMetersPerSecond,
+  //               AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+  //                       .setKinematics(DriveConstants.kDriveKinematics);
 
-        // 2. Generate trajectory
-        Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
-                new Pose2d(0, 0, new Rotation2d(0)),
-                List.of(
-                        new Translation2d(1, 0),
-                        new Translation2d(1, -1)),
-                new Pose2d(2, -1, Rotation2d.fromDegrees(180)),
-                trajectoryConfig);
+  //       // 2. Generate trajectory
+  //       Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
+  //               new Pose2d(0, 0, new Rotation2d(0)),
+  //               List.of(
+  //                       new Translation2d(1, 0),
+  //                       new Translation2d(1, -1)),
+  //               new Pose2d(2, -1, Rotation2d.fromDegrees(180)),
+  //               trajectoryConfig);
 
-        // 3. Define PID controllers for tracking trajectory
-        PIDController xController = new PIDController(AutoConstants.kPXController, 0, 0);
-        PIDController yController = new PIDController(AutoConstants.kPYController, 0, 0);
-        ProfiledPIDController thetaController = new ProfiledPIDController(
-                AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
-        thetaController.enableContinuousInput(-Math.PI, Math.PI);
+  //       // 3. Define PID controllers for tracking trajectory
+  //       PIDController xController = new PIDController(AutoConstants.kPXController, 0, 0);
+  //       PIDController yController = new PIDController(AutoConstants.kPYController, 0, 0);
+  //       ProfiledPIDController thetaController = new ProfiledPIDController(
+  //               AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
+  //       thetaController.enableContinuousInput(-Math.PI, Math.PI);
 
-        // 4. Construct command to follow trajectory
-        SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
-                trajectory,
-                m_robotDrive::getPose,
-                DriveConstants.kDriveKinematics,
-                xController,
-                yController,
-                thetaController,
-                m_robotDrive::setModuleStates,
-                m_robotDrive);
+  //       // 4. Construct command to follow trajectory
+  //       SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
+  //               trajectory,
+  //               m_robotDrive::getPose,
+  //               DriveConstants.kDriveKinematics,
+  //               xController,
+  //               yController,
+  //               thetaController,
+  //               m_robotDrive::setModuleStates,
+  //               m_robotDrive);
 
-        // 5. Add some init and wrap-up, and return everything
-        return new SequentialCommandGroup(
-                new InstantCommand(() -> m_robotDrive.resetPose(trajectory.getInitialPose())),
-                swerveControllerCommand,
-                new InstantCommand(() -> m_robotDrive.stopModules()));
-    }
+  //       // 5. Add some init and wrap-up, and return everything
+  //       return new SequentialCommandGroup(
+  //               new InstantCommand(() -> m_robotDrive.resetPose(trajectory.getInitialPose())),
+  //               swerveControllerCommand,
+  //               new InstantCommand(() -> m_robotDrive.stopModules()));
+    // }
 }
